1917da0a3a70567c672ac9eab837c2dfd2d7e8e2
MohamedAli74 <mohamedali243.123@gmail.com>
2024-12-04 11:43:22 +0200

    start method in Simulation

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -36,8 +36,69 @@ using std::string;
         void Simulation::start()
         {
             open();
-            cout<< "The simulation has started";
+            string currentLine;
+            while(isRunning){
+                getline(cin,currentLine);
+                vector<string> tokens = Auxiliary::parseArguments(currentLine);
+
+                if (tokens[0] == "settlement") {
+            BaseAction *newSettlement = new AddSettlement(tokens[1], static_cast<SettlementType>(stoi(tokens[2])));
+            newSettlement->act(*this);
+            addAction(newSettlement);
+            } 
+            else if (tokens[0] == "facility") {
+
+                BaseAction *newFacility = new AddFacility(tokens[1], static_cast<FacilityCategory>(stoi(tokens[2])),stoi(tokens[3]),stoi(tokens[4]),stoi(tokens[5]), stoi(tokens[6]));
+                newFacility->act(*this);
+                addAction(newFacility);
+            } 
+            else if (tokens[0] == "plan") {
+                BaseAction *newPlan = new AddPlan(tokens[1], tokens[2]);
+                newPlan ->act(*this);
+                addAction(newPlan);
+            } 
+            else if (tokens[0] == "step") {
+                const int numOfStep = stoi(tokens[1]);
+                BaseAction *newStep = new SimulateStep(numOfStep);
+                newStep ->act(*this);
+                addAction(newStep);
+            }
+            else if (tokens[0] == "planStatus") {
+                BaseAction *planStatus = new PrintPlanStatus(stoi(tokens[1]));
+                planStatus ->act(*this);
+                addAction(planStatus);
+            } 
+            else if (tokens[0] == "changePolicy") {
+                BaseAction *changePolicy = new ChangePlanPolicy(stoi(tokens[1]), tokens[2]);
+
+                changePolicy ->act(*this);
+                addAction(changePolicy);
+            } 
+            else if (tokens[0] == "log") {
+                BaseAction *printActionsLog = new PrintActionsLog();
+                printActionsLog ->act(*this);
+                addAction(printActionsLog);
+            }
+            else if (tokens[0] == "close") {
+                BaseAction *close = new Close();
+                close ->act(*this);
+                addAction(close);
+            }  
+            else if (tokens[0] == "backup") {
+                BaseAction *backupSimulation = new BackupSimulation();
+                backupSimulation ->act(*this);
+                addAction(backupSimulation);
+            }  
+            else if (tokens[0] == "restore") {
+                BaseAction *restoreSimulation = new RestoreSimulation();
+                restoreSimulation ->act(*this);
+                addAction(restoreSimulation);
+            } 
+            else {
+            cout << "no comment like this" << endl;
+            }
         }
+    }
         
         void Simulation::addPlan(const Settlement &settlement, SelectionPolicy *selectionPolicy)
         {
